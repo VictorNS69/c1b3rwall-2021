173c67563b40cf579bf65fbbeae2c0cfe0b6e417
Victor Nieves <vnievess@gmail.com>
2021-05-31 22:59:04 +0200

    modified solidity version

--- contracts/self-destruct/Attack.sol
+++ contracts/self-destruct/Attack.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity 0.7.6;
 import "./EtherGame.sol";
 
 contract Attack {
--- contracts/self-destruct/EtherGameFixed.sol
+++ contracts/self-destruct/EtherGameFixed.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity 0.7.6;
 
 /*
     The goal of this game is to be the 3th player to deposit 1 Ether.
