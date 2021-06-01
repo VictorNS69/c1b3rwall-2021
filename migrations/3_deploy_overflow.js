f213acf150feaf725eb845332c50b518a9af4532
Victor Nieves <vnievess@gmail.com>
2021-06-01 13:39:54 +0200

    added OverflowFixed to migration

--- migrations/3_deploy_overflow.js
+++ migrations/3_deploy_overflow.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const TruffleConfig = require('@truffle/config');
 
 const Overflow = artifacts.require('contracts/overflow/Overflow.sol');
+const OverflowFixed = artifacts.require('contracts/overflow/OverflowFixed.sol');
 
 
 let addresses = {};
@@ -21,6 +22,15 @@ module.exports = async function (deployer) {
         await saveAddresesInfo(
             overflow.address,
             "overflow"
+        );
+
+        await deployer.deploy(OverflowFixed);
+        const overflowFixed = await OverflowFixed.deployed();
+
+        console.log('overflowFixed deployed: ', overflowFixed.address)
+        await saveAddresesInfo(
+            overflowFixed.address,
+            "overflowFixed"
         );
 
         await fs.writeFileSync(addressesPath, JSON.stringify(addresses), {flag: 'wx'});
